a1815923f3bf8fcf85a0cfc020388effa34b0288
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-09-07 16:25:49 +0300

    Fix the git repo

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -11,8 +11,9 @@ class TestDataLoader(unittest.TestCase):
      def test_read_csv(self):
         """Test the readcsv method"""
         filename = "../data/train.csv"
+        repo = "https://github.com/Haylemicheal/Pharmaceutical-Sales-Prediction/"
         dataloader = DataLoader()
-        content = dvc.api.read(path=filename, repo="../", rev='v1')
+        content = dvc.api.read(path=filename, repo="https://github.com/Haylemicheal/Pharmaceutical-Sales-Prediction/", rev='v1')
         pd = dataloader.read_csv(io.StringIO(content))
         col1 = pd.columns[0]
         self.assertEqual(col1, "Store")
