c5d73965059ef005da5c4ae79935bc462d3934c2
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-09-07 05:34:05 +0300

    Write a dataloader code

---
+++ scripts/dataloader.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import logging
+import pandas as pd
+
+logging.basicConfig(format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+
+
+class DataLoader:
+    """A class for data loading
+    """
+    def __init__(self):
+        logging.info("The data loader instance is created")
+    
+    def read_csv(self, path):
+        """Read csv file
+           Args:
+                path: location of the csv file
+           Return:
+                df: pandas dataframe
+        """
+        df = pd.read_csv(path)
+        return df
